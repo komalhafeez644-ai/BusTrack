f5fb4f1b98a66ad88bc01b9dee279f4ac3f74846
Ayesha <mamarshmellow27@gmail.com>
2026-06-19 19:00:15 +0500

    Refactor driver module: Added Dashboard, Attendance, Navigation, and Profile screens with Kotlin logic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,36 +19,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-<<<<<<< HEAD
-    }
-}
-
-rootProject.name = "Bustrack"
-=======
-
-        val localProperties = java.util.Properties()
-        val localPropertiesFile = File(rootProject.projectDir, "local.properties")
-        if (localPropertiesFile.exists()) {
-            localProperties.load(localPropertiesFile.inputStream())
-        }
-        val mapboxToken = localProperties.getProperty("MAPBOX_DOWNLOADS_TOKEN") ?: ""
-
-        maven {
-            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-            credentials {
-                // Do not change the username ==> 'mapbox' is correct
-                username = "mapbox"
-                // Use the secret token you created as the password
-                password = mapboxToken
-            }
-        }
     }
 }
 
 rootProject.name = "BusTrack_App"
->>>>>>> 30877b02823e2ae42972719cea4a7ea631857fdd
 include(":app")
  
